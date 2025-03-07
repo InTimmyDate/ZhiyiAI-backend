24f37d0379c9c62a10232317479a251b036f1093
InTimmyDate <timmy.mi.private@gmail.com>
2025-03-07 19:37:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5500;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.json({ limit: '10mb' })); // Increase limit here
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.post("/generate-pattern", async (req, res) => {
   const { genType, prompt, width, height } = req.body;

37ac4d19f28460344d7f096c0526de91235efa11
InTimmyDate <timmy.mi.private@gmail.com>
2025-03-09 20:31:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -9,6 +10,9 @@ app.use(cors());
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+const server = http.createServer(app);
+server.setTimeout(300000); // 300000 ms = 5 minutes
 
 app.post("/generate-pattern", async (req, res) => {
   const { genType, prompt, width, height } = req.body;
